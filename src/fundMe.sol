b56401b21b529e4ced35fc7e9a4f3bea8edfa457
sacredtreasure <selfharm777@protonmail.com>
2023-10-18 19:47:40 +0100

    test-code

---
+++ src/fundMe.sol
@@ -0,0 +1,60 @@
+//SPDX-license-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+error NotOwner();
+
+contract FundMe {
+    using PriceConverter for uint256;
+
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+    //this should be a constant yeah? lmao no mutable asl
+    address public i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+
+    constructor {
+        i_owner = msg_sender;
+    }
+
+    function fund() {
+        require(msg.value.getConversionRate() => MINIMUM_USD, "You probably need to spend a bit more ETH");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You probably need to spend a bit more ETH");
+        addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    modifier onlyOwner {
+        //require(msg.sender == i_owner);
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+
+    function withdraw() public onlyOwner {
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+    }
+
+    fallback() external payable {
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
+    }
+}
