e062ea17a51b067baf612951d12322b335cca01c
sacredtreasure <selfharm777@protonmail.com>
2023-10-20 12:09:30 +0100

    testing before dep(8)

--- script/deployFundMe.s.sol
+++ script/deployFundMe.s.sol
@@ -6,9 +6,10 @@ import {Script} from "lib/forge-std/src/Script.sol";
 import {FundMe} from "src/fundMe.sol";
 
 contract deployFundMe is Script {
-    function run() external {
+    function run() external returns (FundMe){
         vm.startBroadcast();
-        new FundMe();
+       fundMe FundMe =  new FundMe(0x694A1769357215DE4FAC081bf1f309aDC325306);
         vm.stopBroadcast();
+        return FundMe;
     }
 }
